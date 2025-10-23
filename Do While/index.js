7e7797e4d22b389b1c601fbbae841e7891db9f8f
Ylgner Witoszynski Santana <ylgner.wsantana@gmail.com>
2025-10-23 20:00:04 -0300

    Delete Do While directory

--- Do While/index.js
+++
@@ -1,8 +0,0 @@
-let velocidade = 0
-
-do {
-    alert("A velocidade do veículo é " + velocidade + " km/h")
-    velocidade -= 20
-} while (velocidade > 0)
-
-alert("Velocidade final: " + velocidade + " km/h")
