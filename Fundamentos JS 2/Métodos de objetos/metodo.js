ce16e46530010857fc16d02edf3afbab8fa7785d
Ylgner Witoszynski Santana <ylgner.wsantana@gmail.com>
2026-02-18 20:11:18 -0300

    Add files via upload

---
+++ Fundamentos JS 2/Métodos de objetos/metodo.js
@@ -0,0 +1,11 @@
+let pessoa = {
+  nome: "Ylgner",
+  idade: 24,
+  dizerOla() {
+    console.log("Olá, mundo! Meu nome é " + this.nome)
+  }
+}
+
+console.log(pessoa)
+
+pessoa.dizerOla()
---
+++ Fundamentos JS 2/Objetos Globais/objetos.js
@@ -0,0 +1,16 @@
+//O que são?
+/*
+São objetos padrão da linguagem JS que 
+ficam sempre disponíveis para utilizarmos
+
+ATENÇÂO: não confundir com o objeto de escopo global window do navegador
+(ou o escopo global de outro ambientes)
+
+Alguns exemplos: Object, Array, String, Number, Date, Math
+
+Podem ser usados para criar e converter valores
+
+Possuem propriedades e funções úteis e 
+frequentemente necessárias
+*/
+
